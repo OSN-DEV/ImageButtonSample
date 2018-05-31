fffa79e9f6b4ed3b453cd10385fa68c67dc4835f
OSN-DEV <osndev1973@gmail.com>
2018-05-31 23:00:50 +0900

    styleを外部ファイルに切り出し リソースの定義を追加

---
+++ Component/ImageButton.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ImageButtonSample.Component {
+    public class ImageButton : Button {
+
+        static ImageButton() {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ImageButton), new FrameworkPropertyMetadata(typeof(ImageButton)));
+        }
+
+        public static readonly DependencyProperty DefaultImageProperty =
+                                                        DependencyProperty.Register("DefaultImage",                   
+                                                                                    typeof(BitmapImage),                
+                                                                                    typeof(ImageButton),               
+                                                                                    new FrameworkPropertyMetadata());
+        public static readonly DependencyProperty HoverImageProperty =
+                                                        DependencyProperty.Register("HoverImage",
+                                                                                    typeof(BitmapImage),
+                                                                                    typeof(ImageButton),
+                                                                                    new FrameworkPropertyMetadata(null));
+        public static readonly DependencyProperty PressedImageProperty =
+                                                        DependencyProperty.Register("PressedImage",
+                                                                                    typeof(BitmapImage),
+                                                                                    typeof(ImageButton),
+                                                                                    new FrameworkPropertyMetadata(null));
+        public static readonly DependencyProperty DisabledImageProperty =
+                                                        DependencyProperty.Register("DisabledImage",
+                                                                                    typeof(BitmapImage),
+                                                                                    typeof(ImageButton),
+                                                                                    new FrameworkPropertyMetadata(null));
+        public BitmapImage DefaultImage {
+            get { return (BitmapImage)GetValue(DefaultImageProperty); }
+            set { SetValue(DefaultImageProperty, value); }
+        }
+
+        public BitmapImage HoverImage {
+            get { return (BitmapImage)FindResource(GetValue(HoverImageProperty)); }
+            set { SetValue(HoverImageProperty, value); }
+        }
+
+        public BitmapImage PressedImage {
+            get { return (BitmapImage)GetValue(PressedImageProperty); }
+            set { SetValue(PressedImageProperty, value); }
+        }
+
+        public BitmapImage DisabledImage {
+            get { return (BitmapImage)GetValue(DisabledImageProperty); }
+            set { SetValue(DisabledImageProperty, value); }
+        }
+    }
+}
